d3b60d216bdc3682c568be3eb18d740f4663ed5a
Aditya Kumar Behera <128294626+hopexadi@users.noreply.github.com>
2026-04-29 22:53:13 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,3 +1,4 @@
+import logging
 """
 Routes and views for the flask application.
 """
@@ -66,8 +67,10 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning("Invalid login attempt")
             flash('Invalid username or password')
             return redirect(url_for('login'))
+        app.logger.info("admin logged in successfully")
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
